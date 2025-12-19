65adc8a27c5a5e196a0d2cf96c8b272dbcdaa0dc
Yakishev Rashid <businessmanrussia2017@yandex.ru>
2025-12-19 19:35:41 +0500

    Добавил переход от одной вкладки в другую вкладку и обратно

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -14,12 +14,28 @@ class TestConstructor:
     
     def test_click_tabs_scrolls_to_sections(self, driver, tab_locator, section_locator, expected_text):
        
+        driver.find_element(*BurgerIngredientLocators.SAUCES_TAB).click()
+       
+        WebDriverWait(driver, 10).until(
+            expected_conditions.visibility_of_element_located(
+                BurgerIngredientLocators.SAUCES_SECTION
+            )
+        )
+        driver.find_element(*BurgerIngredientLocators.BUNS_TAB).click()
+        BUNS_SECTION = WebDriverWait(driver, 10).until(
+            expected_conditions.visibility_of_element_located(
+                BurgerIngredientLocators.BUNS_SECTION
+            )
+        )
+        assert BUNS_SECTION.is_displayed()
+        assert BUNS_SECTION.text == "Булки"
+         
         element = WebDriverWait(driver, 10).until(
             expected_conditions.element_to_be_clickable(tab_locator)
         )
 
         ActionChains(driver).move_to_element(element).click().perform()
-
+        
         WebDriverWait(driver, 10).until(
             expected_conditions.visibility_of_element_located(section_locator)
         )
